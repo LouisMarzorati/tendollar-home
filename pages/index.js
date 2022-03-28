2bb16656468f7adfb6524334479957d916c33c82
louis marzorati <louismarzorati@gmail.com>
2022-03-28 11:10:31 -0400

    fancy

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
-
+import { useState } from "react";
 export default function Home() {
+	const [fancy, setFancy] = useState(false);
 	return (
 		<div className="container">
 			<Head>
@@ -10,7 +11,43 @@ export default function Home() {
 			</Head>
 
 			<main className="main">
-				<div>home for now</div>
+				<div className={`box ${fancy ? "shadow" : ""}`}>
+					<div className="title">
+						<span>
+							<i>Your site name here!</i>
+						</span>
+						<span onClick={() => setFancy(!fancy)} className="title-button">
+							{fancy ? "TOO FANCY" : "make it fancy"}
+						</span>
+					</div>
+					<div className="body">
+						<div>
+							<span>you provide</span>
+							<ul>
+								<li>
+									<span>$10</span>
+								</li>
+								<li>
+									<span>a couple words</span>
+								</li>
+								<li>
+									<span>maybe a sentence</span>
+								</li>
+							</ul>
+							<span>we provide</span>
+							<ul>
+								<li>
+									<span>a website</span>
+								</li>
+							</ul>
+						</div>
+						<div className="buy-now">
+							<span className={`buy-now-button ${fancy ? "boucing" : ""}`}>
+								buy now
+							</span>
+						</div>
+					</div>
+				</div>
 			</main>
 		</div>
 	);
